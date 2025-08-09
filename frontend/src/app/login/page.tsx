46020e7ae7873002f47b7d70a4a500335bc7a293
MeOldNg <junweing1004@gmail.com>
2025-08-09 18:29:43 +0800

    made marketplace

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import MetaMaskLogin from "../components/MetaMaskLogin";
 
 const login = () => {
