74cc9f242f885de56350d702154d1955816bba53
Mutton9558 <69904687+Mutton9558@users.noreply.github.com>
2025-08-09 19:31:53 +0800

    fixed prediction detail bug

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -463,6 +463,7 @@ const Dashboard = () => {
           const past: any[] = [];
 
           for (let i = 0; i < userMarkets.length; i++) {
+            console.log(userMarkets[i]);
             const marketAddress = await contract.getMarketAddressById(
               userMarkets[i]
             );
@@ -481,7 +482,7 @@ const Dashboard = () => {
               Title: marketTitle,
               PredictionMoney: Number(predictionMoney),
               Tag: marketTag,
-              id: userMarkets[i],
+              id: Number(userMarkets[i].toString()),
             };
 
             if (now <= marketEndTime) {
@@ -538,6 +539,7 @@ const Dashboard = () => {
                   title={prediction.Title}
                   predictionMoney={prediction.PredictionMoney}
                   tag={prediction.Tag}
+                  id={prediction.id}
                 />
               ))}
               {ongoingMarketList.length === 0 && (
@@ -558,6 +560,7 @@ const Dashboard = () => {
                   title={prediction.Title}
                   predictionMoney={prediction.PredictionMoney}
                   tag={prediction.Tag}
+                  id={prediction.id}
                 />
               ))}
               {pastMarketList.length === 0 && <div>No past predictions</div>}
